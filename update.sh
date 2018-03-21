8adf1a0d3d000de2b8ceb00ec9ba9b849b8df45e
IamBusy <1342247033@qq.com>
2018-03-21 20:28:42 +0800

    Fixxx

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 contName=$1
 docker stop ${contName};
 docker rm ${contName};
-docker build -t yugong-${contName} -e app=${contName} .
+docker build -t yugong-${contName} -e "app=${contName}" .
 docker run --name ${contName} -d  yugong-${contName}
